65acadd8a59d82cde4206603d517afa35ec137aa
Thanh Nguyen <puffle.nnt@gmail.com>
2021-12-14 22:19:08 +0700

    done navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-brand">
+                    <NavLink to="/" className="brand">
+                        Tee.
+                    </NavLink>
+                </div>
+                <div className="navbar-nav-left">
+                    <ul>
+                        <li>
+                            <NavLink to="/">
+                                About
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">
+                                Projects
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-nav-right">
+                    <button type='button' className='btn btn-nav'>Let's Talk</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
