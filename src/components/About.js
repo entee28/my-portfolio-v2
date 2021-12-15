2ae062ea6310bb3a1967e636784d875203813ce7
Thanh Nguyen <puffle.nnt@gmail.com>
2021-12-15 23:06:00 +0700

    resume added

--- src/components/About.js
+++ src/components/About.js
@@ -16,11 +16,42 @@ const About = () => {
                 <div className='grid-col-2-about py-64'>
                     <h1 className='heading-text'>Driven by technology</h1>
                     <p className='p-text pl-32'>He considers himself a ‘forever student,’ eager to both build on his technical skills
-                                    and stay in tune with the latest technology through continued research and professional development.
+                        and stay in tune with the latest technology through continued research and professional development.
                     </p>
                 </div>
             </div>
+            <div className='about-container'>
+                <div className="experience" id="experience">
+                    <div className="container">
+                        <header className="section-header text-center">
+                            <p className='p-text'>My Resume</p>
+                            <h2 className='heading-md'>Education, Working Experience</h2>
+                        </header>
+                        <div className="timeline mt-20">
+                            <div className="timeline-item left">
+                                <div className="timeline-text">
+                                    <div className="timeline-date p-text">2020 - 2024</div>
+                                    <h2>Bachelor of Computer and Information Sciences</h2>
+                                    <h4>Ho Chi Minh University of Sciences, HCMC | Auckland University of Technology, NZ</h4>
+                                    <p>
 
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="timeline-item right">
+                                <div className="timeline-text">
+                                    <div className="timeline-date p-text">??? - ???</div>
+                                    <h2>There's more to come</h2>
+                                    <p>
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
