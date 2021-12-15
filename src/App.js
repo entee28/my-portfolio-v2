143672de5413a126d8ff4ca22cd8030d140e74c5
Thanh Nguyen <puffle.nnt@gmail.com>
2021-12-15 10:37:04 +0700

    done hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
     <NavBar/>
+    <Hero/>
       <Switch>
 
       </Switch>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import image from '../img/clip-programming.png'
+import Typical from 'react-typical'
+import chevron from '../img/chevron-right-solid.svg'
+
 
 const Hero = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='hero-container grid-col-2'>
+            <div className='box-text'>
+                <h1 className='mb-9'>Hi,<br /> I'm Thành.</h1>
+                <Typical className="opacity-95 typical-text mb-20" loop={Infinity} wrapper="p"
+                    steps={['HCMUS-AUT Student', 2000, 'Web Developer', 2000, 'Software Engineer', 2000]} />
+                <a href="#" className='btn btn-header'>
+                    <span>See My Projects</span>
+                    <img src={chevron} alt="chevron" />
+                </a>
+            </div>
+            <img src={image} alt="hero" />
+        </section>
     )
 }
 
