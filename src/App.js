03f9c0998d53116d0c8582ae30703d387e47717c
Thanh Nguyen <puffle.nnt@gmail.com>
2021-12-15 15:07:51 +0700

    finished divider component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
+import Divider from './components/Divider';
 
 function App() {
   return (
     <BrowserRouter>
     <NavBar/>
     <Hero/>
+    <Divider/>
       <Switch>
 
       </Switch>
--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import github from '../img/github-brands.svg'
+import envelope from '../img/envelope-regular.svg'
 
 const Divider = () => {
     return (
-        <div>
-            
-        </div>
+        <ul class="divider opacity-50">
+            <li>
+                <a href="https://github.com/entee28" target="_blank">
+                    <img src={github} alt="" className='w-7 h-7' />
+                </a>
+            </li>
+            <li>
+                <a href="mailto:puffle.nnt@gmail.com" target="_blank">
+                    <img src={envelope} alt="" className='w-7 h-7' />
+                </a>
+            </li>
+        </ul>
     )
 }
 
