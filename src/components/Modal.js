4388b8824cedad1e4280638aeeab9541576a3f85
Thanh Nguyen <puffle.nnt@gmail.com>
2021-12-24 16:14:39 +0700

    done form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,13 +14,13 @@ const Modal = ({ showModal, setShowModal }) => {
     })
 
     const closeModal = e => {
-        if(modalRef.current === e.target) {
+        if (modalRef.current === e.target) {
             setShowModal(false);
         }
     };
 
     const keyPress = useCallback(e => {
-        if(e.key === 'Escape' && showModal) {
+        if (e.key === 'Escape' && showModal) {
             setShowModal(false);
         }
     }, [setShowModal, showModal])
@@ -42,7 +42,45 @@ const Modal = ({ showModal, setShowModal }) => {
                                 </button>
                             </div>
                             <div className='form-container'>
-                                <h1 className='heading-md'>Hi, let us talk!</h1>
+                                <h1 className='heading-md mb-20'>Hi, let us talk!</h1>
+                                <form data-netlify="true" className='form-flex' value="sentMessage">
+                                    <input type="hidden" name="form-name" value="contact" />
+
+                                    <div className="input-row">
+                                        <label htmlFor="name" className='input'>
+                                            <input type="text" className="input__field" placeholder=' ' name='name' id='name' />
+                                            <span className="input__label">Name</span>
+                                        </label>
+                                    </div>
+
+                                    <div className="input-row">
+                                        <label htmlFor="mail" className='input'>
+                                            <input type='email' className="input__field" placeholder=' ' name='mail' id='mail' />
+                                            <span className="input__label">Email</span>
+                                        </label>
+                                    </div>
+                                    
+                                    <div className="input-row">
+                                        <label htmlFor="subject" className='input'>
+                                            <input type='text' className="input__field" placeholder=' ' name='subject' id='subject' />
+                                            <span className="input__label">Subject</span>
+                                        </label>
+                                    </div>
+                                    
+                                    
+                                    <div className="input-row">
+                                        <label htmlFor="message" className='input'>
+                                            <textarea className="input__field" placeholder=' ' name='subject' id='subject' />
+                                            <span className="input__label">Message</span>
+                                        </label>
+                                    </div>
+
+                                    <div className="btn-group">
+                                        <button className="btn btn-submit mt-14" type='submit'>Send</button>
+                                        <button className='btn' type='reset'>Reset</button>
+                                    </div>
+
+                                </form>
                             </div>
                         </div>
                     </animated.div>
